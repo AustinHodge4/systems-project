1b515a0bc87849a0d701a0329305c1b060ba1da4
Austin Hodge <hodgeaj4@gmail.com>
2019-03-26 13:36:19 -0400

    fix import

--- deq/urls.py
+++ deq/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.shortcuts import redirect
 from deq.views.homeView import home
 from deq.views.loginView import loginuser, logoutuser
-from deq.views.letterView import letter
+from deq.views.letterView import letterList
 
 from deq.views.facilityView import facilityList, createEditFacility
 from deq.views.checklistview import checklist, createEditChecklist
@@ -11,7 +11,7 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('login/', loginuser),
     path('logout/',logoutuser),
-    path('letters/', letter),
+    path('letters/', letterList),
     path('facilityList/', facilityList),
     path('createEditFacility/', createEditFacility),
     path('checklist/', checklist),
